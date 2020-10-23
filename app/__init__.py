48b8d498235a88f92df3d130f07bca047cde6210
Trung Ly <trunginbox@gmail.com>
2020-10-22 17:36:02 -0700

    fix path to flask's sqlite db file

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+        DATABASE=os.path.join(app.root_path, 'db.sqlite3'),
     )
 
     from . import db
